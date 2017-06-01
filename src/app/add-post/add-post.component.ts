f1fe007a00bcf1c340ea8868bd0d2d9917e23b3f
LianHai Miao <540905936@qq.com>
2017-06-01 23:24:18 +0800

    完成了文章管理模块的添加新文章功能

--- src/app/add-post/add-post.component.ts
+++ src/app/add-post/add-post.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+import { Post } from '../model/post.model';
 
 @Component({
-  selector: 'app-add-post',
-  templateUrl: './add-post.component.html',
-  styleUrls: ['./add-post.component.css']
+    selector: 'app-add-post',
+    templateUrl: './add-post.component.html',
+    styleUrls: ['./add-post.component.css']
 })
+
 export class AddPostComponent implements OnInit {
+    newPost = new Post('', '');
+    constructor(private router: Router) { }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
+    // 提交新文章
+    onSubmit() {
+        this.router.navigate(['/manager/posts']);
+    }
 
+    // 重置文章
+    restPost() {
+      this.newPost = new Post('', '');
+    }
 }
---
+++ src/app/model/post.model.ts
@@ -0,0 +1,8 @@
+class Post {
+    constructor(
+        public title: string,
+        public content: string,
+    ) {  }
+}
+
+export { Post }
